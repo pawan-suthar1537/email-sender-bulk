bb0c2b561db4a2f8f98c71bb067b6de465f8f63d
pawansuthar <pawansuthar15372097023627153@gmail.com>
2024-11-22 17:44:35 +0530

    dfgdfg

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  distDir: "build",
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV !== "development",
+
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
